3c6721b1c076702bbcdbdbc1c0c22f8f170fb4fa
ziaflow <jeremy@ziaflow.com>
2025-01-23 15:56:05 -0700

    Create App2.js

---
+++ src/App2.js
@@ -0,0 +1,6 @@
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "assets/theme";
